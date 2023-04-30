a77c49f0adfd9beb494d14010180a28dac1ed9a5
Abm32 <110992125+Abm32@users.noreply.github.com>
2023-04-30 09:25:35 +0530

    Added generate button

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,15 @@ const Home = () => {
         </div>
         {/* Add this code here*/}
         <div className="prompt-container">
-        <textarea className="prompt-box" placeholder="start typing here" value={userInput} onChange={onUserChangedText}/>;
+        <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText} />
+        {/* New code I added here */}
+          <div className="prompt-buttons">
+          <a className="generate-button" onClick={null}>
+            <div className="generate">
+              <p>Generate</p>
+            </div>
+          </a>
+          </div>
         </div>
       </div>
       <div className="badge-container grow">
